84c4bae0794f7a2437273eb9c1c21fda550cb7b0
Riley Sample <rcs622.rcs@gmail.com>
2022-09-28 15:18:43 -0500

    init project files

---
+++ Player.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+export var player_speed = 100
+
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		$AnimatedSprite.animation = "right"
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+		$AnimatedSprite.animation = "left"
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		$AnimatedSprite.animation = "down"
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		$AnimatedSprite.animation = "up"
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * player_speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
