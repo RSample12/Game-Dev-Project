facc9fbbb7497b2643226b8ea9819effd9221b9c
syemme3 <55899908+syemme3@users.noreply.github.com>
2022-10-03 13:22:43 -0500

    Add files via upload

---
+++ Main.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+export (PackedScene) var Mob
+var score
+
+func _ready():
+	randomize()
+
+#func game_over():
+	#$DeathSound.play()
+  
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$Music.play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
