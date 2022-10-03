292da1234fac4150c878a82fba5e419bdefc7f03
Riley Sample <rcs622.rcs@gmail.com>
2022-10-03 13:34:25 -0500

    fixed player animation

--- Player.gd
+++ Player.gd
@@ -9,6 +9,7 @@ func _ready():
 	
 func _process(delta):
 	player_speed = 200
+	
 	var movement_direction := Vector2.ZERO
 	if Input.is_action_pressed("walk"):
 		player_speed = 100
@@ -25,11 +26,11 @@ func _process(delta):
 		movement_direction.y = -1
 		$AnimatedSprite.animation = "up"
 	
-#	if velocity.length() > 0:
-#		velocity = velocity.normalized() * player_speed
-#		$AnimatedSprite.play()
-#	else:
-#		$AnimatedSprite.stop()
+	if movement_direction.length() > 0:
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
 	movement_direction = movement_direction.normalized()
 	move_and_slide(movement_direction * player_speed)
 	position.x = clamp(position.x, 0, screen_size.x)
