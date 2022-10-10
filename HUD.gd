d328139bc98908e87bbb433f576f9cccf08f85ba
syemme3 <55899908+syemme3@users.noreply.github.com>
2022-10-10 13:56:08 -0500

    Add files via upload

---
+++ HUD.gd
@@ -0,0 +1,38 @@
+extends CanvasLayer
+
+signal start_game
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game Over")
+	yield($MessageTimer, "timeout")
+	$StartButton.show()
+	$MessageLabel.text = "Castlebreaker"
+	$MessageLabel.show()
+	
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	emit_signal("start_game")
+
+func _on_MessageTimer_timeout():
+	$MessageLabel.hide()
--- Main.gd
+++ Main.gd
@@ -12,15 +12,29 @@ var score
 
 func _ready():
 	$Music.play()
-
-#func game_over():
-	#$DeathSound.play()
   
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$Music.play()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$HUD.show_game_over()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func game_over():
+	$ScoreTimer.stop()
+	$DeathSound.play()
+
+func _on_StartTimer_timeout():
+	$ScoreTimer.start()
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_HUD_start_game():
+	pass # Replace with function body.
